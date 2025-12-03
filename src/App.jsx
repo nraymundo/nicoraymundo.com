f20d2514d4955f35527d80872f710becb1bc6f9f
Nico Raymundo <nico@Nicos-MacBook-Air.local>
2025-12-03 00:35:17 -0500

    fix(theme): height

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import './App.css'
-import { Box, useColorModeValue } from "@chakra-ui/react"
-import Header from './sections/Header'
-import Footer from './sections/Footer'
+import "./App.css";
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import Header from "./sections/Header";
+import Footer from "./sections/Footer";
 import { Outlet } from "react-router-dom";
 
 function App() {
   const bg = useColorModeValue("#edede9", "#0A0A0A");
 
   return (
-    <Box bg={bg} width='100%' pt={5}>
+    <Box bg={bg} width="100%" pt={5}>
       <Header />
       <Outlet />
       <Footer />
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,6 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, useColorModeValue } from "@chakra-ui/react";
+
+// const bg = useColorModeValue("#edede9", "#0A0A0A");
 
 const config = {
   initialColorMode: "dark",
@@ -7,13 +9,25 @@ const config = {
 
 const theme = extendTheme({
   config,
-  textStyles: { 
+  textStyles: {
     primary: {
       fontFamily: `'Texturina Variable', sans-serif`,
     },
     secondary: {
       fontFamily: `'Ubuntu', sans-serif`,
       fontWeight: 500,
+    },
+  },
+  styles: {
+    global: {
+      body: {
+        _light: {
+          bg: "#edede9",
+        },
+        _dark: {
+          bg: "#0A0A0A",
+        },
+      },
     },
   },
 });
