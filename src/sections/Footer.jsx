70859fe46fbcde7c5436ad3374cd222dedae78b1
Nico Raymundo <nico@Nicos-MacBook-Air.local>
2025-12-04 20:02:28 -0500

    fix(footer): height

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,11 +1,11 @@
-import { Box, Image } from "@chakra-ui/react"
+import { Box, Image } from "@chakra-ui/react";
 import ContactLinkContainer from "../components/ContactLinkContainer";
 
 export default function Footer() {
   return (
-    <Box w='100%' mt={5} h='150px' pl={[5, 10]} pr={[5, 10]}>
-      <Box h={0.8} mt={5} backgroundColor='#494949'/>
+    <Box w="100%" mt={5} mb={5} pl={[5, 10]} pr={[5, 10]}>
+      <Box h={0.8} mt={5} backgroundColor="#494949" />
       <ContactLinkContainer />
     </Box>
-  )
+  );
 }
