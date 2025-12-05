1a0dcd47085fb76e896a0c07a88b5d2add8085c3
Nico Raymundo <nico@Nicos-MacBook-Air.local>
2025-12-04 21:28:32 -0500

    fix(public): update photo size

--- src/sections/PhotoJournal.jsx
+++ src/sections/PhotoJournal.jsx
@@ -1,9 +1,10 @@
+import { Flex, Image, SimpleGrid, Box, Text, Link } from "@chakra-ui/react";
+import { Link as ReactRouterLink } from "react-router-dom";
 import { photos } from "../utils/photos";
-import PhotoJournalEntry from "../components/PhotoJournalEntry";
 
 const entries = [
   {
-    place: "London & Edinburgh '20",
+    place: "London & Edinburgh",
     date: "03.2020",
     images: photos.lon_edi,
     path: "london-edinburgh",
@@ -36,9 +37,55 @@ const entries = [
 
 export default function PhotoJournal() {
   return (
-    <div className="journal-page">
+    <div className="photo-journal">
       {entries.map((entry) => (
-        <PhotoJournalEntry key={entry.path} entry={entry} />
+        <Box key={entry.place}>
+          <Flex marginTop={20} px={[4, 10]} direction="row">
+            <Link
+              as={ReactRouterLink}
+              to={entry.path}
+              _hover={{ textDecoration: "none", color: "#494949" }}
+              display="inline-flex"
+              alignItems="center"
+              position="relative"
+              zIndex={1}
+            >
+              <Text
+                textStyle="secondary"
+                fontSize={["sm", "xl"]}
+                fontWeight={500}
+              >
+                {entry.place}
+              </Text>
+
+              <Text
+                textStyle="secondary"
+                fontSize={["sm", "xl"]}
+                fontWeight={700}
+                ml={2}
+              >
+                •
+              </Text>
+
+              <Text
+                textStyle="secondary"
+                fontSize={["sm", "xl"]}
+                fontWeight={500}
+                ml={2}
+              >
+                {entry.date}
+              </Text>
+            </Link>
+          </Flex>
+
+          <SimpleGrid columns={[2, 3, 4]} spacing={6} px={[4, 10]} mt={5}>
+            {entry.images.map((img, i) => (
+              <Flex key={img ?? i} justify="center">
+                <Image src={img} fit="contain" w="100%" />
+              </Flex>
+            ))}
+          </SimpleGrid>
+        </Box>
       ))}
     </div>
   );
